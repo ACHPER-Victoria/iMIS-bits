b9b626309152bdac4dc21bd8dced35abf5a6beea
Lee <clam@nyanya.org>
2021-09-13 15:12:21 +1000

    Update certdisplay.sql

--- htmljs/PDF/certdisplay.sql
+++ htmljs/PDF/certdisplay.sql
@@ -2,3 +2,5 @@
 
 /* Staff */
 '<a class="TextButton" href="/user-downloads/certificate-generation/DownloadCertificate.aspx?EventKey=' + vBoCsEvent.EventCode + '&ProdCode=' + vBoACH_OrderLine_Attend.PRODUCT_CODE + '&UID='+ vBoCsRegistration.ShipToId + '">Download</a>'
+
+CASE vBoCsFunction.IsEventRegistrationOption WHEN 1 THEN vBoCsEvent.Title WHEN 0 THEN CONCAT(vBoCsEvent.Title, ' - ', vBoCsFunction.Title) END
