60d2fe0aeb312db5d22b0c4d470206a4d6fe48d0
Clam- <clam@nyanya.org>
2022-11-12 15:12:42 +1100

    Add bulk registration utility

---
+++ htmljs/imports/MassRegEvent/registerEvent.js
@@ -0,0 +1,216 @@
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+var myWorker = new Worker('/common/Uploaded%20Files/Code/MassRegEvent/registerEventWorker.js');
+
+function mergelog(s) {
+  var ta = jQuery('textarea#mergelog');
+  ta.val(ta.val() + s + "\n")
+}
+function updateProgress(i) {
+  jQuery('span#mergecount').text(i);
+}
+function setMaxProgress(i) {
+  jQuery('span#mergetotal').text(i);
+}
+
+const workerMaker = (type, arg) => {
+  // check if a worker has been imported
+  if (window.Worker) {
+    myWorker.postMessage({type, arg});
+  }
+}
+
+myWorker.onmessage = function(ev) {
+  //console.log('Message received from worker');
+  const response = ev.data;
+  const data = response.data;
+  const type = response.type;
+  if (type === 'mergelog') {
+    mergelog(data);
+  } else if (type === 'updateProgress') {
+    updateProgress(data);
+  } else if (type === 'setMaxProgress') {
+    setMaxProgress(data);
+  } else if( type === 'getEvents') {
+    populateEvents(data);
+  } else if( type === 'getHeaders') {
+    handleFilesDone(data);
+  } else if ( type == 'getRegOption') {
+    gotRegOption(data);
+  } else if ( type == 'getMemOption') {
+    gotMemOption(data);
+  } else if( type === 'endProcessing') {
+    endProcessing(data);
+  } else {
+    console.error('An Invalid type has been passed in');
+  }
+}
+CSVFILE = null;
+
+function getEvents() {
+  disableUI(true);
+  jQuery('textarea#mergelog').val("");
+  workerMaker('getEvents', [document.getElementById("__RequestVerificationToken").value]);
+}
+function populateEvents(events) {
+  var selectfield = jQuery("#EventPullDown");
+  jQuery('#EventPullDown option:gt(0)').remove(); // remove all options, but not the first
+  for (let i of events) {
+    selectfield.append(jQuery("<option></option>")
+       .attr("value", i[0]).text(i[1]));
+  }
+  disableUI(false, false);
+}
+
+function changeRegField(event) {
+  var actualevent = jQuery("#EventPullDown").val();
+  if (actualevent) {
+    disableUI(true);
+    workerMaker('getRegOption', [document.getElementById("__RequestVerificationToken").value, CSVFILE, event.target.value, actualevent]);
+  }
+}
+REGOPTIONS = [];
+function gotRegOption(data) {
+  //data is actually a Set and list : regoptions, eventregoptions
+  REGOPTIONS = [];
+  var regoptions = data[0];
+  var eventregoptions = data[1]
+  var listelem = jQuery("#regopts");
+  var index = 0;
+  for (var regopt of regoptions) {
+    REGOPTIONS.push([index,regopt]);
+    var s = jQuery("<select id='RO-{0}'></select>".format(index));
+    s.append(jQuery("<option></option>").attr("value", "").text("(NONE)"));
+    for (var [code, title] of eventregoptions) {
+      s.append(jQuery("<option></option>").attr("value", code).text(title));
+    }
+    listelem.append(jQuery("<li></li>").append(jQuery("<span>{0}</span>".format(regopt)).add(s)));
+    index++;
+  }
+  disableUI(false, true);
+}
+
+function changeMemField(event) {
+  disableUI(true);
+  workerMaker('getMemOption', [document.getElementById("__RequestVerificationToken").value, CSVFILE, event.target.value]);
+}
+function gotMemOption(data) {
+  //data is actually a Set and list : regoptions, eventregoptions
+  var selectfield = jQuery("#memopts");
+  jQuery('#memopts option:gt(0)').remove(); // remove all options, but not the first
+  for (let i of data) {
+    selectfield.append(jQuery("<option></option>")
+       .attr("value", i).text(i));
+  }
+  disableUI(false, true);
+}
+
+
+function handleFiles(flist) {
+  // clear csvfields
+  disableUI(true);
+  jQuery('#csvfields li').remove();
+  // getHeaders
+  CSVFILE = flist[0]
+  workerMaker('getHeaders', [document.getElementById("__RequestVerificationToken").value, CSVFILE]);
+}
+FIELDS = [["iMIS ID", "uid"], ["Registration Option", "regoption"], ["Membership column", "membership"]
+//  ["Email address", "email"], ["Phone", "phone"]
+];
+function handleFilesDone(data) {
+  var headers = data;
+  var listelem = jQuery("#csvfields");
+  for (var i of FIELDS) {
+    // create select list item
+    var s = jQuery("<select id='{0}'></select>".format(i[1]));
+    s.append(jQuery("<option></option>").attr("value", "").text("(NONE)"));
+    for (var h of headers) {
+      s.append(jQuery("<option></option>").attr("value", h).text(h));
+    }
+    if (i[1] == "regoption") {
+      s.change(changeRegField);
+    }
+    if (i[1] == "membership") {
+      s.change(changeMemField);
+    }
+    listelem.append(jQuery("<li></li>").append(jQuery("<span>{0}</span>".format(i[0])).add(s)));
+  }
+  if (headers != null) {
+    document.getElementById("startbutton").removeAttribute("disabled");
+  } else {
+    document.getElementById("startbutton").setAttribute("disabled", "disabled");
+  }
+  disableUI(false);
+}
+
+function disableUI(disable, start=true) {
+  if (disable) {
+    document.getElementById("EventPullDown").setAttribute("disabled", "disabled");
+    document.getElementById("resetbutton").setAttribute("disabled", "disabled");
+    document.getElementById("clist").setAttribute("disabled", "disabled");
+    if (start) { document.getElementById("startbutton").setAttribute("disabled", "disabled"); }
+  } else {
+    document.getElementById("EventPullDown").removeAttribute("disabled");
+    document.getElementById("resetbutton").removeAttribute("disabled");
+    document.getElementById("clist").removeAttribute("disabled");
+    if (start) { document.getElementById("startbutton").removeAttribute("disabled"); }
+  }
+}
+
+function startProcessing() {
+  // allow not everyone and no alliances from to clear a list.
+  jQuery('textarea#mergelog').val("");
+  var fields = {
+    "uid" : jQuery("#uid").val(),
+    "regoption" : jQuery("#regoption").val(),
+    "membership" : jQuery("#membership").val(),
+  }
+  // require output
+  if (!fields["uid"] || !fields["regoption"]) {
+    mergelog("Require ID and Registration Option column.");
+    return;
+  }
+  // build regoptions map...
+  regopts = {};
+  for (var [index,code] of REGOPTIONS) {
+    regopts[code] = jQuery("#RO-{0}".format(index)).val();
+  }
+  var actualevent = jQuery("#EventPullDown").val();
+  if (!actualevent) {
+    mergelog("Need event.");
+    return;
+  }
+  // get membership values if thing...
+  var memopts = [];
+  if (fields["membership"]) {
+    memopts = jQuery("#memopts").val();
+  }
+  var memdate = document.getElementById("memdate").value;
+  // disableUI
+  disableUI(true);
+  workerMaker('startProcessing', [document.getElementById("__RequestVerificationToken").value,
+    CSVFILE, fields, actualevent, regopts, memopts, memdate]);
+}
+
+function endProcessing(data) {
+  mergelog("Done.");
+  disableUI(false, true);
+}
+
+window.addEventListener("load", _ => {
+  getEvents();
+  const d = new Date();
+  const ld = new Date(Date.UTC(d.getFullYear(), d.getMonth()+12, 0));
+  ld.toISOString().split('T')[0]
+  document.getElementById("memdate").value = ld.toISOString().split('T')[0];
+});
---
+++ htmljs/imports/MassRegEvent/registerEventWorker.js
@@ -0,0 +1,507 @@
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+var token = "";
+
+importScripts('/common/Uploaded%20files/Code/utils.js')
+importScripts('/common/Uploaded%20files/Code/papaparse.min.js')
+
+onmessage = function(e) {
+    const data = e.data;
+    const type = data.type;
+    const arg = data.arg;
+    // console.log('Message received from main script');
+    switch (type) {
+      case 'getEvents':
+        token = arg[0];
+        postMessage({
+          type: type,
+          data: getEvents(),
+        });
+        break;
+      case 'getRegOption':
+        token = arg[0];
+        getRegOptions(arg[1], arg[2], arg[3]) //f, column, event
+        break;
+      case 'getHeaders':
+        token = arg[0];
+        getHeaders(arg[1]) //f
+        break;
+      case 'getMemOption':
+        token = arg[0];
+        getMemOptions(arg[1], arg[2]) //f
+        break;
+      case 'startProcessing':
+        token = arg[0];
+        //CSVFILE, fields, actualevent, regopts, memopt, memopts, memdate
+        startProcessing(arg[1], arg[2], arg[3], arg[4], arg[5], arg[6], arg[7]);
+        break;
+      default:
+        console.error('invalid type passed in');
+        break;
+    }
+}
+
+function getEvents() {
+  var tableitems = [];
+  for (let i of apiIterator("/api/Event", [["Status", "P"]])) {
+    tableitems.push([i["EventCode"], "{0} - {1}".format(i["EventCode"], i["Name"])]);
+  }
+  return tableitems;
+}
+
+var COUNT = 0;
+function incrementProgress() {
+  COUNT++;
+  postMessage({
+    type: "updateProgress",
+    data: COUNT,
+  });
+}
+
+function processMember(item, date) {
+  var modified = false;
+  if (genericProp(item, "MemberType") != "VICF") {
+    genericProp(item, "MemberType", "VICF");
+    modified = true;
+  }
+  // check paidthru date:
+  if (genericProp(item, "PaidThrough") < date) {
+    genericProp(item, "PaidThrough", "{0}T00:00:00".format(date));
+    modified = true;
+  }
+  if (modified) {
+    var result = dorequest("/api/CsContact/{0}".format(genericProp(item, "ID")), null, null, [], item, "PUT");
+    if (!result[1]) { mergelog("Error with Mem1: "+ result[1]); return false;}
+  }
+  return true;
+}
+
+function getUserFromID(id, memb, date) {
+  var result = dorequest("/api/CsContact/{0}".format(id));
+  if (result[0]) {
+    if (memb) {
+      if (!processMember(result[1], date)) {
+        return false;
+      }
+    }
+    return id;
+  }
+  else { return null; }
+}
+
+function NewRegWithTemplate(template) {
+  var result = dorequest("/api/EventRegistration/_execute", null, null, [], template);
+  if (!result[0]) { mergelog("Error with New Registration: "+result[1]); return false;}
+  return true;
+}
+function RegisterWithTemplate(template) {
+  var result = dorequest("/api/ComboOrder/_validate", null, null, [], template);
+  if (!result[0]) { mergelog("Error with Reg1: "+result[1]); return false; }
+  // check if validated true
+  if (!result[1]["IsValid"]) { mergelog("Validation fail1:"+result[1]["ValidationResults"]); return false; }
+  // execute the validated entity
+  result = dorequest("/api/ComboOrder", null, null, [], result[1]["Entity"]);
+  if (!result[1]) { mergelog("Error with Reg2: "+ result[1]); return false; }
+  return true;
+}
+
+function CheckAndRegister(eventID, imisID, funccode) {
+  // If registration, check registration option. If same, continue, if not, modify registration I guess ??
+  // If registration and need to change: 1) use template 2) validate 3) post validated body.
+  var result = dorequest("/api/EventRegistration/{0}-{1}".format(eventID, imisID));
+  if (result[0] && "Status" in result[1]) {
+    // get AssociatedInvoiceId
+    var associatedInvoiceID = result[1]["AdditionalAttributes"]["$values"][0]["Value"]
+    // check status.
+    if (result[1]["Status"] == 1) {
+      // check registration Option
+      var regOpt = result[1]["Functions"]["$values"][0]["EventFunction"]["EventFunctionCode"];
+      if (funccode === "") {
+        implementthis;
+      }
+      if (regOpt != funccode) {
+        // modify registration. broken at the moment. FIX PLEASE.
+        mergelog("User: {0} has changed registration. Please manually update their registration from {1} to {2}".format(imisID, regOpt, funccode))
+        return true;
+        //return RegisterWithTemplate(generateTemplate(imisID, eventID, associatedInvoiceID, funccode, regOpt));
+      } else {
+        // registration is fine, no change(s) needed.
+        return true;
+      }
+    } else {
+      //register new registration using template
+      return RegisterWithTemplate(generateTemplate(imisID, eventID, associatedInvoiceID, funccode));
+    }
+  }
+  else { //register new registration
+    return NewRegWithTemplate(generateNewTemplate(eventID, funccode, imisID));
+  }
+}
+
+//CSVFILE, fields, actualevent, regopts
+function startProcessing(f, fields, actualevent, regopts, memopts, memdate) {
+  // ghetto reset count
+  COUNT = -1;
+  incrementProgress();
+  Papa.parse(f, {
+    "header": true,
+    "step": function(r,p) {
+      // check ID
+      var id = null;
+      var field = fields["uid"];
+      if (field && r["data"][0][field].trim()) {
+        // get ID and also update membership while at it I guess ??
+        // check if doing member things...
+        var memb = false;
+        if (fields["membership"] && r["data"][0][fields["membership"]].trim()) {
+          if (memopts.includes(r["data"][0][fields["membership"]].trim())) { memb = true; }
+        }
+        id = getUserFromID(r["data"][0][field].trim(), memb, memdate);
+      }
+      if (id === null) { // didn't find/or don't have ID
+        mergelog("Couldn't find user for ID: "+ r["data"][0][field].trim())
+      } else if (id === false) {
+        mergelog("Stopping due to error."); p.abort();
+        return;
+      }else {
+        // get registration...
+        field = fields["regoption"];
+        var funccode = "";
+        if (field) {
+          funccode = regopts[r["data"][0][field].trim()];
+        }
+        if (!CheckAndRegister(actualevent, id, funccode)) {
+          mergelog("Stopping due to error.");
+          p.abort();
+          return;
+        }
+      }
+      incrementProgress();
+    },
+    "complete": function(r,p) {
+      endProcessing();
+    },
+    "skipEmptyLines": true,
+    "fastMode": false,
+  });
+}
+function endProcessing() {
+  postMessage({
+    type: "endProcessing",
+    data: [],
+  });
+}
+
+function getFunctions(event) {
+  var tableitems = [];
+  if (event) {
+    var result = dorequest("/api/Event/{0}".format(event));
+    if (result[0]) {
+      for (let i of result[1]["RegistrationOptions"]["$values"]) {
+        tableitems.push([i["EventFunctionCode"], "{0} - {1}".format(i["EventFunctionCode"], i["Name"])]);
+      }
+    }
+  }
+  return tableitems;
+}
+
+function getRegOptions(f, column, event) {
+  const regOptions = new Set();
+  // get event regOptions
+  var eventregoptions = getFunctions(event);
+  Papa.parse(f, {
+    "header": true,
+    "step": function(r,p) {
+      var regopt = r["data"][0][column].trim();
+      if (regopt) {
+        regOptions.add(regopt);
+      }
+    },
+    "complete": function(r,p) {
+      sendRegOptions(regOptions, eventregoptions);
+    },
+    "skipEmptyLines": true,
+    "fastMode": false,
+  });
+}
+function sendRegOptions(regoptions, eventregoptions) {
+  postMessage({
+    type: "getRegOption",
+    data: [regoptions, eventregoptions],
+  });
+}
+
+function getMemOptions(f, column) {
+  const memOptions = new Set();
+  Papa.parse(f, {
+    "header": true,
+    "step": (r, p) => {
+      var regopt = r["data"][0][column].trim();
+      if (regopt) {
+        memOptions.add(regopt);
+      }
+    },
+    "complete": (r,p) => { sendMemOptions(memOptions); },
+    "skipEmptyLines": true,
+    "fastMode": false,
+  });
+}
+function sendMemOptions(memoptions) {
+  postMessage({
+    type: "getMemOption",
+    data: memoptions,
+  });
+}
+
+function getHeaders(f) {
+  var rows = 0;
+  var headers = null;
+  Papa.parse(f, {
+    "header": true,
+    "step": function(r,p) {
+      if (headers === null) {
+        headers = r["meta"]["fields"];
+      }
+      rows++;
+    },
+    "complete": function(r,p) {
+      sendHeaders(rows, headers);
+    },
+    "skipEmptyLines": true,
+    "fastMode": false,
+  });
+}
+function sendHeaders(rows, headers) {
+  postMessage({
+    type: "setMaxProgress",
+    data: rows,
+  });
+  postMessage({
+    type: "getHeaders",
+    data: headers,
+  });
+}
+
+function mergelog(s) {
+  postMessage({
+    type: "mergelog",
+    data: s,
+  });
+}
+
+function generateTemplate(imisID, eventID, associatedInvoiceID, funccode, removeCode=null) {
+  var template = JSON.parse(JSON.stringify(DUMBORDERTEMPLATE));
+  template["Order"]["BillToCustomerParty"]["PartyId"] = imisID;
+  template["Order"]["Delivery"]["$values"][0]["CustomerParty"]["PartyId"] = imisID;
+  template["Order"]["Lines"]["$values"][0]["Event"]["EventId"] = eventID;
+  template["Order"]["Lines"]["$values"][0]["AdditionalAttributes"]["$values"][0]["Value"] = associatedInvoiceID;
+  var code = "{0}/{1}".format(eventID, funccode);
+  template["Order"]["Lines"]["$values"][0]["ChildOrderLines"]["$values"][0]["Item"]["ItemCode"] = code;
+  template["Order"]["Lines"]["$values"][0]["ChildOrderLines"]["$values"][0]["Item"]["ItemId"] = code;
+  // if remove code, add on the negative removal function template to the end of childorderlines.
+  if (removeCode != null) {
+    template["Order"]["Lines"]["$values"][0]["ChildOrderLines"]["$values"].unshift(generateNegative(associatedInvoiceID, removeCode));
+  }
+  code = "EVENT-{0}".format(eventID);
+  template["Order"]["Lines"]["$values"][0]["Item"]["ItemCode"] = code;
+  template["Order"]["Lines"]["$values"][0]["Item"]["ItemId"] = code;
+  template["Order"]["OriginatorCustomerParty"]["PartyId"] = imisID;
+  template["Order"]["SoldToCustomerParty"]["PartyId"] = imisID;
+  return template;
+}
+
+DUMBORDERTEMPLATE = {
+  "$type": "Asi.Soa.Commerce.DataContracts.ComboOrderData, Asi.Contracts",
+  "Order": {
+      "$type": "Asi.Soa.Commerce.DataContracts.OrderData, Asi.Contracts",
+      "BillToCustomerParty": {
+          "$type": "Asi.Soa.Commerce.DataContracts.CustomerPartyData, Asi.Contracts",
+          "PartyId": ""
+      },
+      "Delivery": {
+          "$type": "Asi.Soa.Commerce.DataContracts.DeliveryDataCollection, Asi.Contracts",
+          "$values": [
+              {
+                  "$type": "Asi.Soa.Commerce.DataContracts.DeliveryData, Asi.Contracts",
+                  "CustomerParty": {
+                      "$type": "Asi.Soa.Commerce.DataContracts.CustomerPartyData, Asi.Contracts",
+                      "PartyId": ""
+                  },
+                  "DeliveryId": "f7779f90-3d5f-4a8b-8ae5-39dbea026313"
+              }
+          ]
+      },
+      "Lines": {
+          "$type": "Asi.Soa.Commerce.DataContracts.OrderLineDataCollection, Asi.Contracts",
+          "$values": [
+              {
+                  "$type": "Asi.Soa.Events.DataContracts.EventOrderLineData, Asi.Contracts",
+                  "Event": {
+                      "$type": "Asi.Soa.Events.DataContracts.EventSummaryData, Asi.Contracts",
+                      "EventId": ""
+                  },
+                  "AdditionalAttributes": {
+                      "$type": "Asi.Soa.Core.DataContracts.GenericPropertyDataCollection, Asi.Contracts",
+                      "$values": [
+                          {
+                              "$type": "Asi.Soa.Core.DataContracts.GenericPropertyData, Asi.Contracts",
+                              "Name": "AssociatedInvoiceId",
+                              "Value": ""
+                          }
+                      ]
+                  },
+                  "ChildOrderLines": {
+                      "$type": "Asi.Soa.Commerce.DataContracts.OrderLineDataCollection, Asi.Contracts",
+                      "$values": [
+                          {
+                              "$type": "Asi.Soa.Events.DataContracts.EventFunctionOrderLineData, Asi.Contracts",
+                              "Item": {
+                                  "$type": "Asi.Soa.Commerce.DataContracts.ItemSummaryData, Asi.Contracts",
+                                  "ItemClass": {
+                                      "$type": "Asi.Soa.Commerce.DataContracts.ItemClassSummaryData, Asi.Contracts",
+                                      "ItemClassId": "MEETING"
+                                  },
+                                  "ItemCode": "2022NOVCON/T",
+                                  "ItemId": "2022NOVCON/T",
+                              },
+                              "QuantityOrdered": {
+                                  "$type": "System.Nullable`1[[Asi.Soa.Commerce.DataContracts.QuantityData, Asi.Contracts]], mscorlib",
+                                  "Amount": 1.0
+                              },
+                              "QuantityShipped": {
+                                  "$type": "System.Nullable`1[[Asi.Soa.Commerce.DataContracts.QuantityData, Asi.Contracts]], mscorlib",
+                                  "Amount": 1.0
+                              },
+                              "CanCombine": true
+                          }
+                      ]
+                  },
+                  "DeliveryId": "f7779f90-3d5f-4a8b-8ae5-39dbea026313",
+                  "Item": {
+                      "$type": "Asi.Soa.Commerce.DataContracts.ItemSummaryData, Asi.Contracts",
+                      "ItemCode": "EVENT-2022NOVCON",
+                      "ItemId": "EVENT-2022NOVCON",
+                  },
+                  "LineNumber": 1,
+                  "QuantityOrdered": {
+                      "$type": "System.Nullable`1[[Asi.Soa.Commerce.DataContracts.QuantityData, Asi.Contracts]], mscorlib",
+                      "Amount": 1.0
+                  },
+                  "QuantityShipped": {
+                      "$type": "System.Nullable`1[[Asi.Soa.Commerce.DataContracts.QuantityData, Asi.Contracts]], mscorlib",
+                      "Amount": 1.0
+                  }
+              }
+          ]
+      },
+      "OriginatorCustomerParty": {
+          "$type": "Asi.Soa.Commerce.DataContracts.CustomerPartyData, Asi.Contracts",
+          "PartyId": "33276"
+      },
+      "SoldToCustomerParty": {
+          "$type": "Asi.Soa.Commerce.DataContracts.CustomerPartyData, Asi.Contracts",
+          "PartyId": "33276"
+      },
+      "AdditionalAttributes": {
+          "$type": "Asi.Soa.Core.DataContracts.GenericPropertyDataCollection, Asi.Contracts",
+          "$values": [
+              {
+                  "$type": "Asi.Soa.Core.DataContracts.GenericPropertyData, Asi.Contracts",
+                  "Name": "OrderTotalExcludingCredits",
+                  "Value": {
+                      "$type": "System.Decimal",
+                      "$value": 0.0
+                  }
+              }
+          ]
+      }
+  },
+  "Invoices": {
+      "$type": "Asi.Soa.Commerce.DataContracts.InvoiceSummaryDataCollection, Asi.Contracts",
+      "$values": []
+  },
+  "Payments": {
+      "$type": "Asi.Soa.Commerce.DataContracts.RemittanceDataCollection, Asi.Contracts",
+      "$values": []
+  }
+}
+
+function generateNegative(associatedInvoiceID, removecode) {
+  var template = JSON.parse(JSON.stringify(NEGATIVETEMPLATE));
+  template["AdditionalAttributes"]["$values"][0]["Value"] = associatedInvoiceID;
+  template["Item"]["ItemCode"]["ItemId"] = removecode;
+  return template;
+}
+NEGATIVETEMPLATE = {
+  "$type": "Asi.Soa.Events.DataContracts.EventFunctionOrderLineData, Asi.Contracts",
+  "AdditionalAttributes": {
+      "$type": "Asi.Soa.Core.DataContracts.GenericPropertyDataCollection, Asi.Contracts",
+      "$values": [
+          {
+              "$type": "Asi.Soa.Core.DataContracts.GenericPropertyData, Asi.Contracts",
+              "Name": "AssociatedInvoiceId",
+              "Value": ""
+          },
+          {
+              "$type": "Asi.Soa.Core.DataContracts.GenericPropertyData, Asi.Contracts",
+              "Name": "IsEventFunctionCancellation",
+              "Value": {
+                  "$type": "System.Boolean",
+                  "$value": true
+              }
+          }
+      ]
+  },
+  "Item": {
+      "$type": "Asi.Soa.Commerce.DataContracts.ItemSummaryData, Asi.Contracts",
+      "ItemClass": {
+          "$type": "Asi.Soa.Commerce.DataContracts.ItemClassSummaryData, Asi.Contracts",
+          "ItemClassId": "MEETING"
+      },
+      "ItemCode": "2022NOVCON/B",
+      "ItemId": "2022NOVCON/B",
+  },
+  "QuantityOrdered": {
+      "$type": "System.Nullable`1[[Asi.Soa.Commerce.DataContracts.QuantityData, Asi.Contracts]], mscorlib",
+      "Amount": -1.000000
+  },
+  "QuantityShipped": {
+      "$type": "System.Nullable`1[[Asi.Soa.Commerce.DataContracts.QuantityData, Asi.Contracts]], mscorlib",
+      "Amount": -1.000000
+  },
+  "CanCombine": true
+}
+
+function generateNewTemplate(eventID, funccode, imisID) {
+  var template = JSON.parse(JSON.stringify(NEWREGTEMPLATE));
+  template["EventId"] = eventID;
+  var code = "{0}/{1}".format(eventID, funccode);
+  template["RegistrationOptionFunctionId"] = code;
+  template["FunctionId"] = funccode;
+  template["RegistrantId"] = imisID;
+  template["BillTo"] = imisID;
+  return template;
+}
+NEWREGTEMPLATE = {
+  "$type": "Asi.Soa.Events.DataContracts.EventRegistrationRequest, Asi.Contracts",
+  "EntityTypeName": "EventRegistration",
+  "OperationName": "RegisterEvent",
+  "RegistrationType": 3,
+  "EventId": "",
+  "RegistrationOptionFunctionId": "",
+  "FunctionId": "F",
+  "RegistrantId": "",
+  "RegisteredBy": "194",
+  "BillTo": "33276",
+  "Waitlist": false
+}
